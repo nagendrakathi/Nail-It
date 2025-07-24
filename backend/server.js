8819086271fb0b74d1b8a32032a20590befe9b99
Nagendra Kathi <nagendra30244@gmail.com>
2025-07-25 00:01:21 +0530

    Modify server.js

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,12 @@ connectDB()
 
 app.use(express.json());
 //Routes
-app.use('/api/auth', authRoutes);
-app.use("/api/sessions", sessionRoutes)
-app.use("/api/questions", questionRoutes)
+// app.use('/api/auth', authRoutes);
+// app.use("/api/sessions", sessionRoutes)
+// app.use("/api/questions", questionRoutes)
 
-app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 app.use("/uploads", express.static(path.join(__dirname,"uploads"),{}));
 
