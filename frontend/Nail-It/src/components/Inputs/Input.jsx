09b744345beabede4bc0c6b5f64d5b4c6ee1cc6c
Nagendra Kathi <nagendra30244@gmail.com>
2025-07-24 22:57:25 +0530

    Add Login and SignUp pop UI

---
+++ frontend/Nail-It/src/components/Inputs/Input.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+
+const Input = ({ value, onChange, label, placeholder, type }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div>
+      <label className="text-[13px] text-slate-800">{label}</label>
+      <div className="input-box relative flex items-center">
+        <input
+          type={type === "password" ? (showPassword ? "text" : "password") : type}
+          placeholder={placeholder}
+          className="w-full bg-transparent outline-none pr-10" // Add padding-right for icon space
+          value={value}
+          onChange={(e) => onChange(e)}
+        />
+        {type === "password" && (
+          <div className="absolute right-2">
+            {showPassword ? (
+              <FaRegEye
+                size={22}
+                className="text-primary cursor-pointer"
+                onClick={toggleShowPassword}
+              />
+            ) : (
+              <FaRegEyeSlash
+                size={22}
+                className="text-slate-400 cursor-pointer"
+                onClick={toggleShowPassword}
+              />
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
