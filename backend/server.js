70e4480ca29a8fef245c0c04da4970bb06bc10b1
Nagendra Kathi <nagendra30244@gmail.com>
2025-07-26 00:07:22 +0530

    Define authentication routes for Register, Login and Profile

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors=require("cors")
 const path=require("path")
 const connectDB=require("./config/db")
 
+const authRoutes=require('./routes/authRoutes')
+
 const app=express()
 
 app.use(
@@ -18,7 +20,7 @@ connectDB()
 
 app.use(express.json());
 //Routes
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 // app.use("/api/sessions", sessionRoutes)
 // app.use("/api/questions", questionRoutes)
 
