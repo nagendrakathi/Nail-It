da2e0a95d0e1917d95011b5de1b07eb246933a28
Nagendra Kathi <nagendra30244@gmail.com>
2025-07-26 23:49:48 +0530

    Add Session and Questions routes

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,35 @@
-require("dotenv").config()
-const express=require("express")
-const cors=require("cors")
-const path=require("path")
-const connectDB=require("./config/db")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
 
-const authRoutes=require('./routes/authRoutes')
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const questionRoutes = require("./routes/questionRoutes");
 
-const app=express()
+const app = express();
 
 app.use(
-    cors({
-        origin:"*",
-        methods:["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders:["Content-Type", "Authorization"]
-    })
-)
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-connectDB()
+connectDB();
 
 app.use(express.json());
 //Routes
-app.use('/api/auth', authRoutes);
-// app.use("/api/sessions", sessionRoutes)
-// app.use("/api/questions", questionRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
 
 // app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
 // app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
-app.use("/uploads", express.static(path.join(__dirname,"uploads"),{}));
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
-const PORT=process.env.PORT||5000;
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
