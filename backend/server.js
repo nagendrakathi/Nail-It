ad91b4064555f00cb37b7e7dbfd261e808956b1f
Nagendra Kathi <nagendra30244@gmail.com>
2025-07-27 00:45:54 +0530

    Add Gemini API

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
+const { protect } = require("./middlewares/authMiddleware");
+const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
+
 
 const app = express();
 
@@ -26,8 +29,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
 app.use("/api/questions", questionRoutes);
 
-// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
-// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
