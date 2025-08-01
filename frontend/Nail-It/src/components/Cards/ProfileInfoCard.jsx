c24ebfc4be9834b04ea3f13695ed5b0c0c0a183e
Nagendra Kathi <nagendra30244@gmail.com>
2025-08-01 14:16:30 +0530

    Fix 401 error

--- frontend/Nail-It/src/components/Cards/ProfileInfoCard.jsx
+++ frontend/Nail-It/src/components/Cards/ProfileInfoCard.jsx
@@ -11,16 +11,31 @@ const ProfileInfoCard = () => {
     clearUser();
     navigate("/");
   };
-  return user&&(
-    <div className="flex items-center">
-      <img src={user.profileImageUrl} alt="" className="w-11 h-11 bg-gray-300 rounded-full mr-3" />
-      <div>
-        <div className="text-[15px] text-black font-bold leading-3">{user.name || ""}</div>
-        <button className="text-amber-600 text-sm font-semibold cursor-pointer " onClick={handleLogout}>
-          Logout
-        </button>
+  return (
+    user && (
+      <div className="flex items-center">
+        <img
+          src={
+            user.profileImageUrl
+              ? user.profileImageUrl.replace(/^http:\/\//, "https://")
+              : ""
+          }
+          alt=""
+          className="w-11 h-11 bg-gray-300 rounded-full mr-3"
+        />
+        <div>
+          <div className="text-[15px] text-black font-bold leading-3">
+            {user.name || ""}
+          </div>
+          <button
+            className="text-amber-600 text-sm font-semibold cursor-pointer "
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
