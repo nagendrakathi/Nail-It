8040535df6bd782f24aacdad22838c238a5d2413
Nagendra Kathi <nagendra30244@gmail.com>
2025-08-06 13:50:25 +0530

    Modify

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -13,7 +13,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "nail-it/uploads", 
+    folder: "nail-it/uploads",
     allowed_formats: ["jpg", "jpeg", "png"],
     transformation: [{ width: 500, height: 500, crop: "limit" }],
   },
