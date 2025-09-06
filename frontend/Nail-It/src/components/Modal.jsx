d5342ba6922fcf6a6704f12522517614b629197e
Nagendra Kathi <157376864+nagendrakathi@users.noreply.github.com>
2025-09-06 15:03:44 +0530

    Update Modal.jsx

--- frontend/Nail-It/src/components/Modal.jsx
+++ frontend/Nail-It/src/components/Modal.jsx
@@ -1,11 +1,11 @@
 const Modal = ({children, isOpen, onClose, title, hideHeader}) => {
   if(!isOpen) return null;
   return <div className="mx-auto fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black/40">
-    {/* Modal Content */}
+    
     <div 
     className={`relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden`}
     >
-      {/* Modal  Header*/}
+      
       {!hideHeader && (
         <div className="flex items-center justify-between p-4 border-b border-gray-200">
           <h3 className="md:text-lg font-medium text-gray-900">
@@ -33,7 +33,7 @@ const Modal = ({children, isOpen, onClose, title, hideHeader}) => {
               />
           </svg>
       </button>
-      {/* Modal Body (Scrollable) */}
+      
       <div className="flex-1 overflow-y-auto custom-scrollbar">
         {children}
       </div>
